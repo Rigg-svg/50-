fc88add75d0d3b7c795efdf029b66649622127b9
camilo_C <juniorcamiloco0124@gmail.com>
2024-04-03 09:15:25 +0200

    ejercicio1 hecho

---
+++ Ejercicio1/Ejercicio1.js
@@ -0,0 +1,12 @@
+/* Generar un código que
+determine si un numero es par o
+impar
+ */
+let numero = 6
+
+if(numero % 2 == 0){
+    console.log("es par")
+}else{
+    console.log("es impar")
+}
+
---
+++ Ejercicio3.js/Ejercicio3.js
@@ -0,0 +1,9 @@
+/* 3. Generar un código determine la diferencia ( resta ) entre dos números de tal manera que la resta
+siempre se haga del numero mayor hacia el numero menor */
+
+let num1 = 8
+let num2 = 10
+
+
+
+
